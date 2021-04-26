ef051d76b4914058f7d444b939fe1b8132878c77
Che' Goodridge <>
2021-04-26 00:17:01 -0400

    Added routing

--- src/Content.js
+++ src/Content.js
@@ -1,131 +1,118 @@
 //  import logo from './logo.svg';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Card from '@material-ui/core/Card';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import CardActions from '@material-ui/core/CardActions';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import IconButton from '@material-ui/core/IconButton';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import Reservations from './components/Reservations';
+import Contact from './components/Contact';
+import About from './components/About';
+import Cart from './components/Cart';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './css/style.css';
+import './css/materialize.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 // import { Button, Card, Row, Col } from 'react-materialize';
+library.add(fab);
+
+  const Content = () => (
+    <div className="App" >
+      <Router>
+        <header>
+
+          <nav className="nav-col">
+            <div className="nav-wrapper">
+              <a href="#!" className="brand-logo">Logo</a>
+              <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+              <ul className="right hide-on-med-and-down">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/menu">Menu</Link></li>
+                <li><Link to="/reservations">Reservations</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/cart"><i class="material-icons">shopping_basket</i></Link></li>
+              </ul>
+            </div>
+          </nav>
+
+          <ul className="sidenav" id="mobile-demo">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/menu">Menu</Link></li>
+                <li><Link to="/reservations">Reservations</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/cart"><i class="material-icons">shopping_basket</i></Link></li>
+          </ul>
+        </header>
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  details: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  content: {
-    flex: '1 0 auto',
-  },
-  cover: {
-    width: 151,
-  },
-  controls: {
-    display: 'flex',
-    alignItems: 'flex-end',
-    justifyContent: 'space-around',
-    
-    paddingLeft: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
-  },
-  controlCounters: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/menu">
+            <Menu />
+          </Route>
+          <Route path="/reservations">
+            <Reservations />
+          </Route>
+          <Route path="/contact">
+            <Contact/>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
 
-  },
-  playIcon: {
-    height: 38,
-    width: 38,
-  },
+      </Router>
 
-  formSize:{
-    width: 20
-  },
-  cardRadius:{
-    borderRadius: 10,
-  },
+      <footer className="page-footer footer-col">
+        <div className="row">
+            <div className="col s12 m6 l3 center-align">
+              <img id="footer-logo" src="./img/Logo.png" alt="Site Logo"></img>
+              <h5 className="white-text">King Triton's Seafood Palace</h5>
+              <p>(246)439-9000</p>
+              <p>info@triton.com</p>
+            </div>
 
-  gridContent:{
-    display: 'flex',
-    justifyContent: 'center'
-  }
-}));
+            <div className="col s12 m6 l3 center-align">
+              <h5 className="white-text">Opening Hours</h5>
+              <p>Mon - Sat: 11AM - 4PM</p>
+              <p>Sunday: Closed</p>
+            </div>
 
-function Content() {
+            <div className="col s12 m6 l3 center-align">
+              <h5 className="white-text">Location</h5>
+              <p>Hastings Main Road Ch Ch</p>
+            </div>
 
-  const classes = useStyles();
-  const theme = useTheme();
+            <div className="col s12 m6 l3 center-align">
+              <h5 className="white-text center-align">Follow Us</h5>
+              <div className="row center-align" style={{padding: "8px"}}>
+                <a href="twitter.com" style={{padding: "8px"}}><FontAwesomeIcon icon={['fab','twitter']} size="2x" /></a>
+                <a href="twitter.com" style={{padding: "8px"}}><FontAwesomeIcon icon={['fab','instagram']} size="2x" /></a>
+                <a href="twitter.com" style={{padding: "8px"}}><FontAwesomeIcon icon={['fab','facebook']} size="2x" /></a>
+              </div>
+            </div>
 
-  const cardInfo = [
-    {image: "img/mains/shrimpalfredo.jpg", name: "Shrimp Alfredo", price:16,},
-    {image: "img/mains/lobster-mac.jpg", name: "Lobster Mac & Cheese", price:22,},
-    {image: "img/mains/fried-calamari.jpeg", name: "Fried Calamari", price:16,},
-    {image: "img/mains/fried-calamari.jpeg", name: "Fried Calamari", price:16,},
+        </div>
 
-];
-
-const renderCard = (card, index) => {
-    return(
+        <div class="footer-copyright">
+          <div class="container center-align">
+          © 2021 King Triton's Seafood Palace
+          </div>
+        </div>
+      </footer>
       
-        <Grid item xs={12} sm={3}>
-          <Card className="card small" style={{borderRadius: "20px"}} key={index}>
-              <CardMedia
-              component="img"
-              alt="Contemplative Reptile"
-              height="200"
-              image={card.image}
-              title="Contemplative Reptile"
-              className="card-image"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {card.name} 
-                </Typography>
-              </CardContent>
-              <CardActions className={classes.controls}>
-                  <Box className={classes.controlCounters}>
-                    <IconButton size="small" color="primary">
-                      <RemoveIcon />
-                    </IconButton>
-                    <TextField id="standard-size-small" className={classes.formSize} defaultValue="1" size="small" />
-                    <IconButton size="small" color="primary">
-                      <AddIcon />
-                    </IconButton>
-                  </Box>
-                  <Fab color="primary" aria-label="add">
-                    <AddIcon />
-                  </Fab>
-              </CardActions>
-          </Card>
-        </Grid>
-
-    )
-};
-
-  return (
-    <div className="App">
-      <Grid container direction="row" className={classes.gridContent}>
-        {cardInfo.map(renderCard)}
-      </Grid>
 
     </div>
   );
-}
+
 
 export default Content;
---
+++ src/components/Reservations.js
@@ -0,0 +1,164 @@
+//  import logo from './logo.svg';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from '@material-ui/core/Card';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import IconButton from '@material-ui/core/IconButton';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from '@material-ui/icons/Remove';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import { createMuiTheme } from '@material-ui/core/styles';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// import { Button, Card, Row, Col } from 'react-materialize';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    justifyContent: 'space-around',
+    
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  },
+  controlCounters: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    
+
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
+
+  formSize:{
+    width: 20,
+    paddingTop: 0,
+    paddingBottom: 0,
+    textAlign: 'center'
+  
+  },
+  cardRadius:{
+    borderRadius: 10,
+  },
+
+  gridContent:{
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}));
+
+function Reservations() {
+
+  const classes = useStyles();
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#2196f3',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
+
+    const cardInfo = [
+        {image: "img/mains/shrimpalfredo.jpg", name: "Shrimp Alfredo", price:16,},
+        {image: "img/mains/lobster-mac.jpg", name: "Lobster Mac & Cheese", price:22,},
+        {image: "img/mains/fried-calamari.jpeg", name: "Fried Calamari", price:16,},
+        {image: "img/mains/fried-calamari.jpeg", name: "Fried Calamari", price:16,},
+
+    ];
+
+    const renderCard = (card, index) => {
+        return(
+        
+            <Grid item xs={12} sm={3}>
+            <Card className="card small" style={{borderRadius: "20px"}} key={index}>
+                <CardMedia
+                component="img"
+                alt="Contemplative Reptile"
+                height="200"
+                image={card.image}
+                title="Contemplative Reptile"
+                className="card-image"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2" align="left">
+                        {card.name} 
+                    </Typography>
+                </CardContent>
+                <CardActions className={classes.controls}>
+                    <Box className={classes.controlCounters}>
+                        <IconButton size="small" color="primary">
+                        <RemoveIcon />
+                        </IconButton>
+                        <TextField id="filled-basic" className={classes.formSize} textAlign={'center'} defaultValue="1" size="small" />
+                        <IconButton size="small" color="primary">
+                        <AddIcon />
+                        </IconButton>
+                    </Box>
+                    <Fab color="primary" aria-label="add">
+                        <img src="img/mdi_basket-plus.png"></img>
+                    </Fab>
+                </CardActions>
+            </Card>
+            </Grid>
+
+        )
+    };
+
+    return (
+        <div className="App" theme={theme}>
+        <Container maxWidth="lg">
+            <Typography gutterBottom variant="h3" component="h2" align="left">
+                    Menu
+            </Typography>
+        </Container>
+
+            <Grid container direction="row" className={classes.gridContent}>
+            {cardInfo.map(renderCard)}
+            </Grid>
+
+        </div>
+    );
+}
+
+export default Reservations;
